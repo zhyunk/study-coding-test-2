4a8cbbe8746acfc29c345ee6ddab1e3211ea0bc2
zhyunk <gimwlgus@gmail.com>
2026-05-02 15:39:04 +0900

    ✏️ 연습장

---
+++ kotlinStudy/introduction/study/test.kt
@@ -0,0 +1,36 @@
+package kotlinStudy.introduction.study
+
+import kotlin.math.max
+
+fun main() {
+// .fold
+//    val nums = intArrayOf(101,2,33,4,5,6,77,8,9)
+//
+//    println("최대값 구하기: ${nums.fold(100) { acc, i -> max(acc, i) }}")
+//    println("숫자 이어붙이기: ${nums.fold("") { acc, i -> acc + i }}")
+
+
+
+// .reduce
+//    val nums = intArrayOf(1,2,3,4,5,6,7,8,9)
+//
+//    println("최대값 구하기: ${nums.reduce { acc, i -> max(acc, i) }}")
+//    println("요소들의 곱 구하기: ${nums.reduce { acc, i -> acc * i }}")
+
+
+// .partition
+//    val nums = intArrayOf(1,2,3,4,5,6,7,8,9)
+//    val (odd, even) = nums.partition { it % 2 == 1 }
+//
+//    println("odd: $odd")
+//    println("even: $even")
+}
+
+
+
+
+
+
+
+
+
