9dfbe3422a4be9a5dec66e233ccc4b5e441e697a
zhyunk <gimwlgus@gmail.com>
2026-05-10 20:51:34 +0900

    ✅ 프로그래머스 - 코틀린 기초 24일차

---
+++ kotlinStudy/introduction/day24/Q004.kt
@@ -0,0 +1,41 @@
+package kotlinStudy.introduction.day24
+
+/**
+https://school.programmers.co.kr/learn/courses/30/lessons/181834?language=kotlin
+ *문제 설명
+ * 알파벳 소문자로 이루어진 문자열 myString이 주어집니다.
+ * 알파벳 순서에서 "l"보다 앞서는 모든 문자를 "l"로 바꾼 문자열을 return 하는 solution 함수를 완성해 주세요.
+ *
+ * 제한사항
+     * 1 ≤ myString ≤ 100,000
+     * myString은 알파벳 소문자로 이루어진 문자열입니다.
+ * 입출력 예
+     * myString	    result
+     * "abcdevwxyz"	"lllllvwxyz"
+     * "jjnnllkkmm"	"llnnllllmm"
+ * 입출력 예 설명
+     * 입출력 예 #1
+         * 0 ~ 4번 인덱스의 문자 "a","b","c","d","e"는 각각 "l"보다 앞서는 문자입니다. 따라서 "l"로 고쳐줍니다.
+         * 그 외의 문자는 모두 "l"보다 앞서지 않는 문자입니다. 따라서 바꾸지 않습니다.
+         * 따라서 "lllllvwxyz"을 return 합니다.
+     * 입출력 예 #2
+         * 0번, 1번, 6번, 7번 인덱스의 문자 "j","j","k","k"는 각각 "l"보다 앞서는 문자입니다. 따라서 "l"로 고쳐줍니다.
+         * 그 외의 문자는 모두 "l"보다 앞서지 않는 문자입니다. 따라서 바꾸지 않습니다.
+         * 따라서 "llnnllllmm"을 return 합니다.
+ */
+
+class Solution244 {
+    fun solution(myString: String): String {
+        return myString.replace(Regex("[a-l]"), "l")
+    }
+    /*
+    fun solution(myString: String): String {
+        return myString.toCharArray().joinToString("") { if (it < 'l') "l" else it.toString() }
+    }
+    */
+}
+
+fun main() {
+    println(Solution244().solution("abcdevwxyz"))
+    println(Solution244().solution("jjnnllkkmm"))
+}
